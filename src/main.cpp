f202d63210b30b178e0455dcb9156b1e21021894
ThanhZ <89237062+thqnhz@users.noreply.github.com>
2026-02-22 01:53:50 +0700

    feat: basic window drawing, added assets/LICENSE.assets

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
-#include <print>
+/* Copyright (C) 2026-present ThqnhZ
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * */
+
+
+#include "raylib.h"
 
 int main() {
-    std::print("Hello, world!\n");
+    ::InitWindow(1280, 720, "Rogueket Defense");
+    while (!::WindowShouldClose()) {
+        ::BeginDrawing();
+        ::ClearBackground(::BLACK);
+        ::DrawText("Rogueket Defense", 0, 0, 32, ::RAYWHITE);
+        ::EndDrawing();
+    }
     return 0;
 }
 
